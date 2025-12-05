c18fc849e300902ec73518b9bd3db87f95b72fe1
Kayleexx <sinhamitali56@gmail.com>
2025-12-05 19:07:07 +0530

    fix(lexer): rely on logos Err for invalid lexemes; remove Token::Error catch-all

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use sauce::lexer::Lexer; 
+
 fn main() {
-    println!("Hello, world!");
+
+    let code = "grab vibe = 100 |> yell";
+
+    println!("Sauce Lexer Debug\n");
+    println!("Input: '{}'", code);
+
+    let lexer = Lexer::new(code);
+
+    for result in lexer {
+        match result {
+            Ok(spanned) => {
+                println!(
+                    " [{:?}..{:?}] {:?}", 
+                    spanned.span.start, 
+                    spanned.span.end, 
+                    spanned.token
+                );
+            }
+            Err(e) => {
+                println!(" Error: {}", e);
+            }
+        }
+    }
 }
